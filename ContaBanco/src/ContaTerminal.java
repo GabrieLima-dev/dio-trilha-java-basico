90abfb303c2574e7b2231a44bd896a0861d0d04f
Gabriel Lima <gabrieldesouzalima@gmail.com>
2023-08-28 15:20:41 -0300

    initial ProcessoSeletivo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,26 +1,12 @@
 import java.util.Locale;
 import java.util.Scanner;
-
 public class ContaTerminal {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+    public static void main(String[] args){
+        for(int x=1;x<=10;x++){
+            if(x % 2==0) //% operador que representa módulo de uma divisão
+                System.out.println(x);
+        }
 
-        System.out.println("Por favor, digite o número da Conta! ");
-        int numeroConta = sc.nextInt();
-
-        System.out.println("Por favor, digite o número da Agência! ");
-        String agencia = sc.next();
-
-        System.out.println("Por favor, digite o seu nome! ");
-        String nomeCliente = sc.next();
-
-        System.out.println("Por favor, digite o seu saldo! ");
-        double saldo = sc.nextDouble();
-
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
-                + agencia + ", conta " + numeroConta + " e R$ " + saldo
-                + " já está disponível para saque.");
-
-        sc.close();
     }
 }
+
